0451b32361e70b4998defa2751e362e7c1da265e
IH-M2 <ilhae.na@gmail.com>
2024-12-04 02:20:09 +0900

    add ExceptionHandler example

--- rest-api/src/test/java/com/example/rest_api/RestApiApplicationTests.java
+++ rest-api/src/test/java/com/example/rest_api/RestApiApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.rest_api;
 
+import com.example.rest_api.model.UserRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestApiApplicationTests {
 
+	@Autowired
+	ObjectMapper objectMapper;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JsonProcessingException {
+		UserRequest user = new UserRequest();
+		user.setUserName("steve");
+		user.setUserAge(10);
+		user.setEmail("ste@mail.com");
+		user.setIsKorean(true);
+
+		String json = objectMapper.writeValueAsString(user);
+		System.out.println(json);
+
+		UserRequest dto = objectMapper.readValue(json, UserRequest.class);
+		System.out.println("dto");
 	}
 
 }
